7a593a6714b412c778587a53d8e143c638c35bcf
Anees Haider <anees.haider@tenpearls.com>
2015-04-23 10:33:58 +0500

    --First Commit

--- data/app.js
+++ data/app.js
@@ -2,3 +2,10 @@
  * Created by asjad.saboor on 4/23/2015.
  */
 var app = angular.module('accountingApp', ['']);
+
+app.controller('userCtrl',function($scope,$http){
+
+    $http.get("../../data/users.json").then(
+        function(response) { $scope.users = response.data;});
+
+});
